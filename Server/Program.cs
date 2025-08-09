22ba97235f3faea685bbe18048dd44005ea1086d
yunus <yunsemreclk@gmail.com>
2025-08-09 13:48:34 +0300

    Add database connection configuration and DbContext setup

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ECContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
