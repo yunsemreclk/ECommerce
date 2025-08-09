d8301a8fddc17780d32b34dc99b430a9e34485cb
yunus <yunsemreclk@gmail.com>
2025-08-09 13:30:19 +0300

    Add Swagger UI configuration

--- Server/Program.cs
+++ Server/Program.cs
@@ -12,6 +12,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "API V1");
+    });
 }
 
 app.UseHttpsRedirection();
