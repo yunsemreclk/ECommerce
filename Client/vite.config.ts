a6d2f71a39a09e585bb19f201f21ed0a40e2669e
yunus <yunsemreclk@gmail.com>
2025-08-12 11:22:47 +0300

    Configure Vite server settings with port and strictPort options

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   plugins: [react()],
 })
